a93293804d57aac778b72d992f20bb54d7b7c371
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2022-07-02 12:11:26 -0400

    Clean up provider directory header

--- src/components/providers/ProviderDirectoryHeader.js
+++ src/components/providers/ProviderDirectoryHeader.js
@@ -1,19 +1,16 @@
 import './ProviderDirectoryHeader.scss';
-import { IoLocationSharp } from 'react-icons/io5';
+import LocationPin from '../../assets/icons/location-pin.svg';
 
 const ProviderListHeader = () => {
   return (
-    <header className="provider-directory-header">
-      <div className="provider-directory-header__container">
-        <h1 className="provider-directory-header__heading">
-          Browse our providers
-        </h1>
-        <p className="provider-directory-header__sub-heading">
-          Mental Wellness
-        </p>
-        <div className="provider-directory-header__location">
-          <IoLocationSharp />
-          <span className="provider-directory-header__location---text">ON</span>
+    <header className="directory-header">
+      <div className="directory-header__container">
+        <h1 className="directory-header__heading">Browse our providers</h1>
+        <p className="directory-header__sub-heading">Mental Wellness</p>
+        {/* TO DO: Make Badge component reusable */}
+        <div className="directory-header__location">
+          <img src={LocationPin} alt="location-pin" />
+          <span className="directory-header__location--text">ON</span>
         </div>
       </div>
     </header>
